bde2f4357eaa12a6fdeec349cc80ef6d1769196c
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-09-19 21:33:36 -0300

    Chaning value from varchar and adding more space around keys

--- bd_peliculas.sql
+++ bd_peliculas.sql
@@ -2,16 +2,16 @@ CREATE DATABASE peliculas;
 
 CREATE TABLE peliculas(
     id INT,
-    pelicula VARCHAR(70),
+    pelicula VARCHAR(200),
     año_estreno INT,
-    director VARCHAR(30),
-    PRIMARY KEY(id)
+    director VARCHAR(200),
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE reparto(
     id INT,
-    actor VARCHAR(50),
-    FOREIGN KEY(id) REFERENCES peliculas(id)
+    actor VARCHAR(200),
+    FOREIGN KEY (id) REFERENCES peliculas (id)
 );
 
 \COPY peliculas FROM '/Users/mac/Desktop/desafio_top_100/peliculas.csv' csv header
